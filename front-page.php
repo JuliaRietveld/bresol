5aa1c4396b4c60c916b29bddf74631b20d100b34
MrsColombo <info@juliarietveld.nl>
2016-10-04 19:07:21 +0200

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+*
+* @package bresol
+* @author Julia Rietveld
+* @link http://juliarietveld.nl
+* @copyright Copyright (c) 2016, Julia Rietveld
+* @license GPL-2.0+
+*/
+
+genesis();
---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+*
+* @package bresol
+* @author Julia Rietveld
+* @link http://juliarietveld.nl
+* @copyright Copyright (c) 2016, Julia Rietveld
+* @license GPL-2.0+
+*/
+load_child_theme_textdomain('bresol');
+
+add_action('genesis_setup', 'bresol_setup');
+function bresol_setup() {
+
+}
+
+//genesis();
